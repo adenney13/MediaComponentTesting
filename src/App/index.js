debe5eccdbc979f78806d4383722d3d7492ffc1d
Aaron Denney <aarondenney13@gmail.com>
2019-07-21 11:59:17 -0400

    set state so that display adjusts with width. Will begin to set some break points now

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,43 @@ import "./style.css";
 import Media from "react-media"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      width: null
+    }
+  }
+
+componentDidMount() {
+  this.updateWindowWidth();
+  window.addEventListener('resize', this.updateWindowWidth)
+}
+
+componentWillUnmount() {
+  window.removeEventListener('resize', this.updateWindowWidth)
+}
+
+updateWindowWidth = () => {
+  this.setState ({
+    width: window.innerWidth
+  })
+}
+
+// screenWidth = (evt) => {
+//   this.setState ({
+//     width: Number(evt.target.value)
+//   })
+// }
+
   render() {
+    console.log(this.state.width)
     return (
     <div className="App">
-      <Media query="(max-width: 750px)">
+      <Media query="(width)">
         {matches => 
           matches? (
-            <p>This document is less than 750px</p>
+            <p>This document is {this.state.width}px wide</p>
           ) : (
             <p>JK...this document is more than 750px</p>
           )
